18b8b1f3f6e71ca832b195e8d028762086a29d65
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2021-07-18 20:57:32 -0300

    Removing user routes from app.js to refactor the routes

--- src/app.js
+++ src/app.js
@@ -9,15 +9,4 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
-app.get('/users', (req, res) => {
-  const users = [
-    { name: 'John Doe', mail: 'john@mail.com' },
-  ];
-  res.status(200).json(users);
-});
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
-});
-
 module.exports = app;
