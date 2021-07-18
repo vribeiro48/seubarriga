0ddd03d9b5c5ad447ac63c2ed7cc4f1f812eee63
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2021-07-18 20:58:43 -0300

    Integrating the routes into the app via consign

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const consign = require('consign');
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app);
 
 app.get('/', (req, res) => {
