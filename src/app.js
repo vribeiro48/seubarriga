492a7f494fbb88aa3e964a9e0a37cca2b3ea37c8
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2021-07-20 20:28:37 -0300

    Implementing Knex Logger for eventual debbuging needs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+// const knexLogger = require('knex-logger');
 const knexfile = require('../knexfile');
 
 // TODO criar chaveamento dinâmico
 app.db = knex(knexfile.test);
+
+// app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
