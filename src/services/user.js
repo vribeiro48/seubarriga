a3481cf64181a5291eb26cce589f4e4e2e32e374
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2021-07-20 21:00:04 -0300

    Throwing errors for each user field that is left blank

--- src/services/user.js
+++ src/services/user.js
@@ -3,7 +3,14 @@ module.exports = (app) => {
     return app.db('users').where(filter).select();
   };
 
-  const save = (user) => {
+  const save = async (user) => {
+    if (!user.name) return { error: 'Nome é um atributo obrigatório' };
+    if (!user.email) return { error: 'Email é um atributo obrigatório' };
+    if (!user.passwd) return { error: 'Senha é um atributo obrigatório' };
+
+    const userDb = await findAll({ email: user.email });
+    if (userDb && userDb.length > 0) return { error: 'Já existe um usuário com esse email' };
+
     return app.db('users').insert(user, '*');
   };
 
